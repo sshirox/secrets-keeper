70a4dee66b52b972ab2169c53792c1d487db8ac7
Maksim Shirokov <sshirox@yandex.ru>
2025-02-26 21:57:14 +0500

    Init server routes

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,1 +1,43 @@
-package server
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+
+	"github.com/sshirox/secrets-keeper/internal/database"
+	"github.com/sshirox/secrets-keeper/internal/handlers"
+	auth "github.com/sshirox/secrets-keeper/internal/middleware"
+)
+
+func main() {
+	_ = godotenv.Load()
+	database.ConnectDatabase()
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Post("/register", handlers.Register)
+	r.Post("/login", handlers.Login)
+
+	r.Route("/vault", func(r chi.Router) {
+		r.Use(auth.AuthMiddleware)
+
+		r.Post("/", handlers.AddVaultEntry)
+		r.Get("/", handlers.GetVaultEntries)
+		r.Delete("/{id}", handlers.DeleteVaultEntry)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("The server is running on port: ", port)
+	http.ListenAndServe(":"+port, r)
+}
--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.23.0
 toolchain go1.23.6
 
 require (
+	github.com/go-chi/chi/v5 v5.2.1
 	github.com/go-chi/jwtauth/v5 v5.3.2
 	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.34.0
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
@@ -14,7 +16,6 @@ require (
 
 require (
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
-	github.com/go-chi/chi/v5 v5.2.1 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
